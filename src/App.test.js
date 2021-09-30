f546a92374bc439f86b47f2d4ac5e0946e70eab5
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-09-30 13:00:46 +0200

    App.jsx tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,31 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
+import { ListContextProvider } from "./context/list/ListContext";
 
-test('renders learn react link', () => {
+/**
+ * 3 As of Testing
+ * Arrange:
+ * Is to set up the stage to this particular test. In our frontend case means to mount a component.
+ * Act:
+ * Do something in order to expect some behaviour.
+ * Assert:
+ * To confirm or denny that whatever we did in the "Act" part was the expected behaviour
+ */
+
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Welcome/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+test("Should show the welcome screen if the list is empty", () => {
+  const fakeData = [];
+  Storage.prototype.getItem = jest.fn(() => JSON.stringify(fakeData));
+  render(
+    <ListContextProvider>
+      <App />
+    </ListContextProvider>
+  );
+  const textElement = screen.getByText(/EIKA's shopping list/i);
+  expect(textElement).toBeInTheDocument();
 });
