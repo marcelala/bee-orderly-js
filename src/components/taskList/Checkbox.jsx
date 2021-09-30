294d91cde4957072d124f7676564c773e6d5dee3
Marcela F <54597344+marcelala@users.noreply.github.com>
2021-09-30 14:56:07 +0200

    Checkbox.jsx tests

--- src/components/taskList/Checkbox.jsx
+++ src/components/taskList/Checkbox.jsx
@@ -1,12 +1,13 @@
 export default function Checkbox({ checked, onChange }) {
-	return (
-		<div className="checkbox-wrapper">
-			<input
-				type="checkbox"
-				className="status"
-				checked={checked}
-				onChange={onChange}
-			/>
-		</div>
-	);
+  return (
+    <div className="checkbox-wrapper">
+      <input
+        type="checkbox"
+        className="status"
+        data-testid="checkbox"
+        checked={checked}
+        onChange={onChange}
+      />
+    </div>
+  );
 }
---
+++ src/components/taskList/Checkbox.test.js
@@ -0,0 +1,26 @@
+// npm Packages
+import { render, screen, fireEvent } from "@testing-library/react";
+// Project files
+import Checkbox from "./Checkbox";
+
+test("Should fire an event on change", async () => {
+  const mockMethod = jest.fn();
+  render(<Checkbox onChange={mockMethod} />);
+  const inputCheckboxElement = screen.getByTestId("checkbox");
+  fireEvent.click(inputCheckboxElement);
+  expect(mockMethod).toHaveBeenCalledTimes(1);
+});
+
+test("Should be checked when status is true", async () => {
+  const mockMethod = jest.fn();
+  render(<Checkbox checked={true} onChange={mockMethod} />);
+  const inputCheckboxElement = screen.getByTestId("checkbox");
+  expect(inputCheckboxElement.checked).toBe(true);
+});
+
+test("Should not be checked when status is false", async () => {
+  const mockMethod = jest.fn();
+  render(<Checkbox checked={false} onChange={mockMethod} />);
+  const inputCheckboxElement = screen.getByTestId("checkbox");
+  expect(inputCheckboxElement.checked).toBe(false);
+});
